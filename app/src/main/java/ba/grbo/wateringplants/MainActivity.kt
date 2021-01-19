63d6930aed121a653474d00f2cda3f1d47f2c275
Jasmin Grbo <grbo.dev@gmail.com>
2021-01-19 23:26:57 +0100

    Initial commit

---
+++ app/src/main/java/ba/grbo/wateringplants/MainActivity.kt
@@ -0,0 +1,11 @@
+package ba.grbo.wateringplants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
